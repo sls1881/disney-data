312d380ff02df787fad9d2d4cb2770dc9e4642a2
‘Shelby <sls1881@gmail.com>
2021-02-18 15:56:38 -0800

    forgot to save

--- app.js
+++ app.js
@@ -13,14 +13,14 @@ const { disneyData } = require('./data.js');
 //Add cors... for reasons
 app.use(cors());
 
-//We define an endpoint with a path and then callback the request and response
+//We define an endpoint with a path and then callback the request and response: response should be the data const
 app.get('/disney', (req, res) => {
     res.json({ results: disneyData });
 });
 
 app.get('/disney/:id', (req, res) => {
     const id = Number(req.params.id);
-
+    //the object is the data
     const selectDisney = disneyData.find((disney) => disney.id === id);
 
     res.json({ results: selectDisney });
