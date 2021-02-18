0acb0c8aa24f7cf6e01db965bdb3c33774417221
‘Shelby <sls1881@gmail.com>
2021-02-18 15:55:43 -0800

    data is rendering

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-//Import cors
-const cors = require('cors');
-
 //Import express library 
 const express = require('express')
+
+//Import cors
+const cors = require('cors');
 
 //Define out routs for our endpoint
 const app = express();
@@ -14,14 +14,19 @@ const { disneyData } = require('./data.js');
 app.use(cors());
 
 //We define an endpoint with a path and then callback the request and response
-app.get('/', (req, res) => {
-    res.json('results: home')
-})
-
 app.get('/disney', (req, res) => {
-    res.json('results: disney')
-})
+    res.json({ results: disneyData });
+});
+
+app.get('/disney/:id', (req, res) => {
+    const id = Number(req.params.id);
+
+    const selectDisney = disneyData.find((disney) => disney.id === id);
+
+    res.json({ results: selectDisney });
+});
+
 
 module.exports = {
     app
-}
+};
